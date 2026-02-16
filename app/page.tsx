0184b5d221a77ec66dfe0188e22359622855b821
Gauravkashyap043 <kashyapgaurav460@gmail.com>
2026-02-16 19:25:05 +0530

    Clean project structure

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-4xl font-bold text-yellow-400">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  );
+}
