e6d4dc0f70c6a6ad30d89341c887467446d395dd
Gauravkashyap043 <kashyapgaurav460@gmail.com>
2026-02-16 19:47:50 +0530

    This was testing/merging commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-black">
       <h1 className="text-4xl font-bold text-yellow-400">
-        Tailwind is Working 🚀
+        Tailwind is Working And git branch merging is also working 🚀🔥
       </h1>
     </div>
   );
